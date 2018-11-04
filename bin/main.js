6fde842eeb23f0eba968814c01ef59a1deed64f9
周雄 <zhouxiong03@meituan.com>
2018-11-04 11:36:30 +0800

    chore(all) 0.2.0新版本

--- bin/main.js
+++ bin/main.js
@@ -6,7 +6,7 @@ var path = require('path');
 var kxcli = require('./kxcli');
 
 program
-    .version('0.1.0')
+    .version('0.2.0')
     .option('-m, --module <value>', '创建新的模块', create)
     .parse(process.argv);
 
